7965b3c5757c8f7534e51021e196775abe0a6d90
maingate <josansnim@gmail.com>
2018-03-28 15:49:47 +0900

    Post 관련 화면 추가

---
+++ ios-realestate/Presentation/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  ios-realestate
+//
+//  Created by JungMoon-Mac on 2018. 3. 28..
+//  Copyright © 2018년 JungMoon. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ios-realestate/Presentation/PostViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PostViewController.swift
+//  ios-realestate
+//
+//  Created by JungMoon-Mac on 2018. 3. 28..
+//  Copyright © 2018년 JungMoon. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class PostViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func setupUI(){
+        
+    }
+    
+    func setupBind(){
+        
+    }
+}
